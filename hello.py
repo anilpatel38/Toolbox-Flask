1bad338c5c5d3e730d0ad3fa7929100d765a4bc8
anilpatel38 <anil.patel@students.olin.edu>
2017-03-28 00:44:41 -0400

    submitting code for toolbox

--- hello.py
+++ hello.py
@@ -1,13 +1,30 @@
 """
-Simple "Hello, World" application using Flask
+Flask app toolbox code
 """
 
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/profile', methods=['POST', 'GET'])
+def profile():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+
+        if len(firstname) > 0 and len(lastname) > 0:
+            return render_template('profile.html', firstname = firstname, lastname = lastname)
+        else:
+            return render_template('error.html')
+
 
 if __name__ == '__main__':
     app.run()
